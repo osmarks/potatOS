1e2ed259fc2540c9e47f7814743a6ea9534189ad
osmarks <osmarks@protonmail.com>
2020-08-24 14:17:58 +0100

    improve log viewer

--- src/main.lua
+++ src/main.lua
@@ -1245,12 +1245,20 @@ end
 )
 		]],
 		["/rom/programs/log.lua"] = [[
-local old_mode = ... == "old"
+local args = table.concat({...}, " ")
 local logtext
-if old_mode then
+if args:match "old" then
 	logtext = potatOS.read "old.log"
 else
 	logtext = potatOS.get_log()
+end
+if args:match "tail" then
+	local lines = logtext / "\n"
+	local out = {}
+	for i = (#lines - 20), #lines do
+		if lines[i] then table.insert(out, lines[i]) end
+	end
+	logtext = table.concat(out, "\n")
 end
 textutils.pagedPrint(logtext)
 		]],
